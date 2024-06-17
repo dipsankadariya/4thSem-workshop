f26deed10ed17f03efc9cd31a8575836b8e448c7
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-06-17 21:52:52 +0545

    updates

--- DAY2/sample1/lib/main.dart
+++ DAY2/sample1/lib/main.dart
@@ -1,125 +1,141 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApplication());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApplication extends StatelessWidget {
+  const MyApplication({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "HealthSphere",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          backgroundColor: Colors.blue[900], // Dark blue for app bar background
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 40),
+              Text(
+                "Welcome to HealthSphere",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[900], // Dark blue for headings
+                ),
+              ),
+              SizedBox(height: 20),
+              const Text(
+                "HealthSphere is your go-to application for quick access to essential medical information during emergencies and beyond. ",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w100,
+                  color: Colors.black87, // Dark grey for body text
+                ),
+              ),
+              SizedBox(height: 5),
+              const Text(
+                "Designed with simplicity and accessibility in mind, HealthSphere empowers users with straightforward yet comprehensive medical insights at their fingertips.",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w100,
+                  color: Colors.black87, // Light grey for additional text
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Key Features:",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[900], // Dark blue for headings
+                ),
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20),
+                  Text(
+                    "1. Regular updates and tips on maintaining a healthy lifestyle.",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w100,
+                      color: Colors.black87, // Dark grey for body text
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "2. Quick access to essential medical information.",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w100,
+                      color: Colors.black87, // Dark grey for body text
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "3. User-friendly interface for easy navigation.",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w100,
+                      color: Colors.black87, // Dark grey for body text
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "4. Emergency contact information.",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w100,
+                      color: Colors.black87, // Dark grey for body text
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor:
+              Colors.blue[900], // Dark blue for floating action button
+          child: const Icon(
+            Icons.home_filled,
+            size: 30,
+            color: Colors.white,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blue[900], // Dark blue for bottom app bar
+          child: Container(
+            height: 50.0,
+            child: const Center(
+              child: Text(
+                "© 2024 HealthSphere. All Rights Reserved.",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.0,
+                ),
+              ),
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
