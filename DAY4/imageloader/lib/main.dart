5d1ac9c4664c862ad872a8feb3f23847dbafbacf
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-06-19 19:09:13 +0545

    new updates

--- DAY4/imageloader/lib/main.dart
+++ DAY4/imageloader/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const ImageLoaderApp());
+  runApp(myApp());
 }
 
-class ImageLoaderApp extends StatelessWidget {
-  const ImageLoaderApp({super.key});
+class myApp extends StatelessWidget {
+  const myApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,67 +22,27 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomePage> createState() => _HomePage();
 }
 
-class _HomePageState extends State<HomePage> {
-  String photoPath = "assets/images/ticktack.png";
-  TextEditingController textController = TextEditingController();
-
-  void loadImage() {
-    setState(() {
-      photoPath = "assets/images/" + textController.text;
-    });
-  }
-
+class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(163, 7, 121, 234),
-        title: const Text(
-          "Image Loader",
-          style: TextStyle(color: Colors.white, fontSize: 30),
+        title: Text(
+          "Image loader",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
+        backgroundColor: Colors.amber,
         centerTitle: true,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
             height: 400,
             width: 400,
-            decoration: BoxDecoration(
-              border: Border.all(width: 1, color: Colors.orange),
-            ),
-            child: Image.asset(photoPath,
-                errorBuilder: (context, error, stackTrace) {
-              return Center(
-                child: Text(
-                  'Image not found',
-                  style: TextStyle(fontSize: 24, color: Colors.red),
-                ),
-              );
-            }),
-          ),
-          SizedBox(height: 50),
-          Container(
-            width: 500,
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: TextField(
-              controller: textController,
-              decoration: InputDecoration(
-                labelText: "Enter image name (e.g., ticktack.png)",
-                labelStyle: TextStyle(color: Colors.orangeAccent, fontSize: 14),
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: loadImage,
-            child: Text("Show"),
+            child: Image.asset("assets/images/ticktack.png"),
           ),
         ],
       ),
