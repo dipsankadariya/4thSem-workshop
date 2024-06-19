46747416410455e3e38606d8b0dceef0fe0a7f52
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-06-19 21:30:58 +0545

    new updates

--- DAY4/rolladice/lib/main.dart
+++ DAY4/rolladice/lib/main.dart
@@ -1,31 +1,90 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Roll a  Dice",
-      home: homePage(),
+      debugShowCheckedModeBanner: false,
+      title: "Roll a Dice",
+      home: HomePage(),
     );
   }
 }
 
-class homePage extends StatefulWidget {
-  const homePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<homePage> createState() => _homePage();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _homePage extends State<homePage> {
+class _HomePageState extends State<HomePage> {
+  final Random _random = Random();
+  int diceNum = 1;
+
+  void rollDice() {
+    setState(() {
+      diceNum = _random.nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Roll a dice",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 400,
+              height: 400,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2.0),
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              padding: EdgeInsets.all(8.0),
+              child: Image.asset(
+                "images/${diceNum}.jpg", // assets halda
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Dice Rolled : ${diceNum}",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(255, 81, 143, 174),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: rollDice,
+              child: Text("Roll"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
